0db48e9bc0b10a0ff7c85a2e1ef84014b1b45f0e
Eldad <eldadf456@gmail.com>
2024-04-03 22:55:10 +0300

    Commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import GuestList from "./state/GuestList";
+import UserSearch from "./state/UserSearch";
 // import  {Parent} from "./props/Parent"
 const App  = () => {
     return (
         <div>
             <GuestList/>
+            <UserSearch/>
             {/*<Parent/>*/}
         </div>
     );
---
+++ src/state/UserSearch.tsx
@@ -0,0 +1,33 @@
+import {useState} from "react";
+
+const users = [
+    {name: 'Sarah', age: 20},
+    {name: 'Alex', age: 20},
+    {name: 'Micheal', age: 20}
+]
+export const UserSearch: React.FC = () => {
+    const [name, setName] = useState("");
+    const [user, setUser] = useState<{name:String, age: number} | undefined>();
+
+    const onClick = () => {
+        const foundUser =
+            users.find((user) => {
+                return user.name === name;
+        });
+        setUser(foundUser)
+    }
+    return (
+        <div>
+
+            <input value={name} onChange={(e) => setName(e.target.value)} />
+            <button onClick={onClick}>Click Me</button>
+            <div>
+                <h3>{user && user.name}</h3>
+                {user && user.age}
+            </div>
+        </div>
+    )
+
+}
+export default UserSearch;
+
